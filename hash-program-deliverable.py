bcd2bbb6c04ff48690a6d76bafd6e5dbb9d7b5d4
James Karwowski-Mulready <james.karwowski@outlook.com>
2026-02-05 09:49:25 -0500

    Add SHA-256 calculation function
    Fix syntax error in file opening and add SHA-256 calculation function.

--- hash-program-deliverable.py
+++ hash-program-deliverable.py
@@ -2,3 +2,13 @@ import hashlib
 import json
 import os
 
+"""Calculates the SHA_256 of the file path"""
+def calc_sha256(file_path):
+  sha256_hash = hashlib.sha256()
+  try:
+    with open(file_path, "rb" as f:
+      for byte_block in iter(lambda: f.read(4096), b""):
+        sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+  except Exception:
+    return None
